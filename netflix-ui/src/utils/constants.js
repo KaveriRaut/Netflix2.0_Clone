88df8d630c8cbfb42d6878b75337c579d0b51ecf
Kaveri Raut <97447480+KaveriRaut@users.noreply.github.com>
2022-12-27 13:21:48 +0530

    Update constants.js

--- netflix-ui/src/utils/constants.js
+++ netflix-ui/src/utils/constants.js
@@ -1,5 +1,5 @@
 //this file only to store the API key and its url
 
-export const API_KEY = "a19376533b9b5fa9f69db2d48edf2b46";
+export const API_KEY = "a19376533b9b5fa9f69db2d48edf2b46654613134654"; //enter your API_KEY
 
 export const TMBD_BASE_URL = "https://api.themoviedb.org/3";
